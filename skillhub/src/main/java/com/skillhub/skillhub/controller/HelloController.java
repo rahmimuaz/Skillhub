7e531453d71fc7583335b07fff15ecdea5627979
rahmimuaz <rahmimuaz10@gmail.com>
2025-04-12 22:03:59 +0530

    updatez

--- skillhub/src/main/java/com/skillhub/skillhub/controller/HelloController.java
+++ skillhub/src/main/java/com/skillhub/skillhub/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String greet() {
-        return "sample";
+        return "welcome to Skillhub";
     }
     
 }
