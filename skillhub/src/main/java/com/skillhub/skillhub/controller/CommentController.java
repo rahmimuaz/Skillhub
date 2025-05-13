6ee6168d9beb9aa21c874b0d4af1de069f5a85b8
Baranger Benjamin <baranger369@gmail.com>
2025-05-13 15:44:57 +0530

    Update CommentController.java

--- skillhub/src/main/java/com/skillhub/skillhub/controller/CommentController.java
+++ skillhub/src/main/java/com/skillhub/skillhub/controller/CommentController.java
@@ -1,42 +1,64 @@
-package com.skillhub.skillhub.controller;   
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+package com.skillhub.skillhub.controller;
 
 import com.skillhub.skillhub.model.Comment;
 import com.skillhub.skillhub.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("/api/comments")
+@CrossOrigin(origins = "*")
 public class CommentController {
 
     @Autowired
     private CommentService commentService;
 
-    @PostMapping("/{postId}")
-    public Comment addComment(@PathVariable String postId, @RequestBody Comment comment) {
-        return commentService.addComment(postId, comment);
+    @PostMapping
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment) {
+        return ResponseEntity.ok(commentService.createComment(comment));
     }
 
-    @GetMapping("/{postId}")
-    public List<Comment> getCommentsByPostId(@PathVariable String postId) {
-        return commentService.getCommentsByPostId(postId);
+    @PostMapping("/{parentId}/reply")
+    public ResponseEntity<Comment> createReply(
+            @PathVariable String parentId,
+            @RequestBody Comment reply) {
+        return ResponseEntity.ok(commentService.createReply(parentId, reply));
     }
 
-    @GetMapping("/{postId}/{id}")
-    public Optional<Comment> getCommentById(@PathVariable String postId, @PathVariable String id) {
-        return commentService.getCommentById(postId, id);
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<List<Comment>> getCommentsByPostId(@PathVariable String postId) {
+        return ResponseEntity.ok(commentService.getCommentsByPostId(postId));
     }
 
-    @PutMapping("/{postId}/{id}")
-    public Comment updateComment(@PathVariable String postId, @PathVariable String id, @RequestBody Comment comment) {
-        return commentService.updateComment(postId, id, comment);
+    @GetMapping("/post/{postId}/top-level")
+    public ResponseEntity<List<Comment>> getTopLevelComments(@PathVariable String postId) {
+        return ResponseEntity.ok(commentService.getTopLevelComments(postId));
     }
 
-    @DeleteMapping("/{postId}/{id}")
-    public void deleteComment(@PathVariable String postId, @PathVariable String id) {
-        commentService.deleteComment(postId, id);
+    @GetMapping("/{parentId}/replies")
+    public ResponseEntity<List<Comment>> getReplies(@PathVariable String parentId) {
+        return ResponseEntity.ok(commentService.getReplies(parentId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Comment> updateComment(
+            @PathVariable String id,
+            @RequestBody Comment updatedComment) {
+        return ResponseEntity.ok(commentService.updateComment(id, updatedComment));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable String id) {
+        commentService.deleteComment(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/post/{postId}")
+    public ResponseEntity<Void> deleteCommentsByPostId(@PathVariable String postId) {
+        commentService.deleteCommentsByPostId(postId);
+        return ResponseEntity.ok().build();
     }
 }
